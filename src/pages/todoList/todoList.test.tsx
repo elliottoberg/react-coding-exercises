f9b51ce99c3ba9a0c7093737b227fedfd0246b3b
elliottoberg <elliott@productplan.com>
2024-03-28 11:37:35 -0700

    chore: remove sample projects

--- src/pages/todoList/todoList.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import TodoList from './todoList';
-
-const component = <TodoList />;
-describe('TodoList component', () => {
-  it('renders correctly', () => {
-    render(component);
-  });
-});
